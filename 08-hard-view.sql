77a7cf717f52c871d430833fba55ed89049d6393
Egor <57009978+trueuser3@users.noreply.github.com>
2023-12-22 18:28:05 +0300

    Update 08-hard-view.sql

--- 08-hard-view.sql
+++ 08-hard-view.sql
@@ -17,8 +17,8 @@ order by sum(dish_price) desc;
 
 
 -- (2) Создали view, в котором отсортировали стоимости заказов по убыванию и вывели работников, которые эти заказы делали
-create or replace view the_best_employee15 as
-select order_id, employee_name, "SUM" - discount_amount
+create or replace view the_best_employee as
+select order_id, employee_name, "SUM" - discount_amount as discount
 from (
 select distinct order_id, employee_name, discount_amount,
 string_agg(dish_name, ', ') over(partition by order_id) as "Total",
@@ -30,22 +30,23 @@ join shawarma.dish using (dish_entry_id)
 order by "SUM" desc) as payment_nodiscount;
 
 --Посмотрели, что получилось
-select * from the_best_employee15;
+select * from the_best_employee;
 
 
 
--- (3) Создали view, в котором написаны адреса кафе и стоимости заказов, сделанных в них
+-- (3) Создали view, в котором написаны адреса кафе и стоимости заказов с учетом скидки, их содержание и всё это отсортировали по убыванию.
 create or replace view the_best_cafe as 
-select cafe_address, payment_amount
-from cafe
-join employee e using(cafe_id)
-join shawarma.order using(employee_id);
+select distinct cafe_address, "Total", "SUM" - discount_amount as discount
+from(select distinct cafe_id, cafe_address, discount_amount, dish_price,
+string_agg(dish_name, ', ') over(partition by order_id) as "Total",
+sum(dish_price) over(partition by order_id) as "SUM"
+from shawarma.cafe
+join shawarma.employee e using(cafe_id)
+join shawarma.order using(employee_id)
+join shawarma.dish_x_order using(order_id)
+join shawarma.dish using (dish_entry_id)
+order by "SUM" desc) as payment_nodiscount
+order by discount desc;
 
 --Вновь посмотрели, что получилось
 select * from the_best_cafe;
-
---Отсортируем кафе от самого прибыльного до самого НЕприбыльного
-select cafe_address, sum(payment_amount)
-from the_best_cafe
-group by cafe_address
-order by sum(payment_amount) desc; 
